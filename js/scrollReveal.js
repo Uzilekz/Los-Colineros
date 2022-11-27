c03b9d7226c4aaf0b0d7f60c3f9b40906d5b71f5
Uzilekz <perezaguerregerardo2004@gmail.com>
2022-11-27 15:16:26 -0300

    Finished

---
+++ js/scrollReveal.js
@@ -0,0 +1,48 @@
+// TITLES
+ScrollReveal().reveal('.sub-heading, .heading', {
+    scale: 0.77, duration: 2500, interval: 300, delay: 0
+});
+
+// HERO
+ScrollReveal().reveal('.main__hero__content img, .main__hero__content .btn', {
+    reset: "true", scale: 0.85, duration: 2000, delay: 0
+});
+
+// ABOUT
+ScrollReveal().reveal('.row .image img, .row .content h4, .row .content p, .row .content .btn, .row .icons', {
+    scale: 0.77, duration: 2000, interval: 250, delay: 0
+});
+
+// REVIEWS
+ScrollReveal().reveal('.mySwiper', {
+    scale: 0.85, duration: 2500, delay: 0
+});
+
+// MENU
+ScrollReveal().reveal('.menu__btn', {
+    scale: 0.77, duration: 2000, interval: 150, delay: 0
+});
+ScrollReveal().reveal('#bebida .box', {
+    scale: 0.85, duration: 2000, delay: 0
+});
+
+// EQUIPO
+ScrollReveal().reveal('.main__team__cards__card', {
+    scale: 0.85, duration: 1500, interval: 200, delay: 0
+});
+
+// PEDIR
+ScrollReveal().reveal('.main__order form', {
+    scale: 0.85, duration: 2000, delay: 0
+});
+ScrollReveal().reveal('.main__order form .inputBox, .order__submit', {
+    scale: 0.85, duration: 1000, delay: 300, interval: 300
+});
+
+// FOOTER
+ScrollReveal().reveal('.footer .box', {
+    scale: 0.77, duration: 2500, interval: 250, delay: 0
+});
+ScrollReveal().reveal('.footer__txt', {
+    scale: 0.17, duration: 2000, delay: 0
+});
